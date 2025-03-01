58146df3fc2172f53d98c25c5d3ee77981e5f102
anas yahia <anas.yahya42@gmail.com>
2025-03-01 14:44:19 +0100

    refactor: update sign-up form and validation messages; implement registration and login methods in AuthService

--- src/app/core/auth/components/sign-up/sign-up.component.ts
+++ src/app/core/auth/components/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { ValidationMessageComponent } from "../../../../shared/components/validation-message/validation-message.component";
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,22 +10,48 @@ import { ValidationMessageComponent } from "../../../../shared/components/valida
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  private readonly authservice = inject(AuthService)
+
   authForm = new FormGroup({
-    fName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    lName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/)]),
-    confirmPassword: new FormControl(''),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern(/^[a-zA-Z0-9!@#$%^&*]{6,16}$/)]),
+    rePassword: new FormControl(''),
     acceptTerms: new FormControl(false, [Validators.requiredTrue])
   }, { validators: this.passwordMatchValidator });
 
   passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
 
-    return control.get('password')?.value === control.get('confirmPassword')?.value ? null : { passwordMismatch: true };
+    return control.get('password')?.value === control.get('rePassword')?.value ? null : { passwordMismatch: true };
   }
 
   onSubmit() {
-    console.log(this.authForm.value);
+    if (this.authForm.valid) {
+      this.authservice.register(this.authForm.value).subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
+    }
+
+
+
+
+
+  }
+
+
+  showPassword = false;
+  showrePassword = false;
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  togglerePassword() {
+    this.showrePassword = !this.showrePassword;
   }
 
 }
--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { register } from 'module';
+import { env } from 'process';
+import { environments } from '../../../../environments/environments.prod';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private HttpClient: HttpClient) { }
+
+  register(data: any): Observable<any> {
+    return this.HttpClient.post(environments.baseUrl + 'auth/signup', data);
+  }
+  login(data: any): Observable<any> {
+    return this.HttpClient.post(environments.baseUrl + 'auth/signin', data);
+  }
 }
+
+
